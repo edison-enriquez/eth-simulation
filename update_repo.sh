0c22347e1ab701eb498170b63a6190cee04f61cb
edison-enriquez <62826864+edison-enriquez@users.noreply.github.com>
2024-05-26 20:54:03 +0000

    Update

--- update_repo.sh
+++ update_repo.sh
@@ -2,22 +2,26 @@
 
 # Función para mostrar el uso del script
 function show_usage() {
-  echo "Uso: $0 [-b nombre_de_la_rama] [-c 'mensaje del commit']"
+  echo "Uso: $0 [-b nombre_de_la_rama] [-c 'mensaje del commit'] [-u 'url del repositorio remoto']"
   exit 1
 }
 
 # Valores por defecto
 branch_name="main"
 commit_message="Actualizando carpeta eth_forge"
+remote_url=""
 
 # Parsear los argumentos
-while getopts ":b:c:" opt; do
+while getopts ":b:c:u:" opt; do
   case $opt in
     b)
       branch_name=$OPTARG
       ;;
     c)
       commit_message=$OPTARG
+      ;;
+    u)
+      remote_url=$OPTARG
       ;;
     \?)
       echo "Opción inválida: -$OPTARG" >&2
@@ -31,17 +35,34 @@ while getopts ":b:c:" opt; do
 done
 shift $((OPTIND -1))
 
+# Verifica si se proporcionó una URL del repositorio remoto
+if [ -z "$remote_url" ]; then
+  show_usage
+fi
+
 # Inicializa Git LFS si no se ha hecho antes
 git lfs install
 
-# Agregar la carpeta eth_forge al área de staging
+# Asegurar que los archivos grandes están siendo rastreados por LFS
+git lfs track "*.hdf5"
+git lfs track "*.AppImage"
+
+# Eliminar archivos grandes del historial
+git filter-repo --path demo.hdf5 --path ethereum/ganache-2.7.1-linux-x86_64.AppImage --invert-paths
+
+# Configurar el origen del repositorio
+git remote remove origin
+git remote add origin "$remote_url"
+
+# Agregar los archivos y .gitattributes para LFS
+git add .gitattributes
 git add eth_forge/
 
 # Crea un commit con el mensaje proporcionado
 git commit -m "$commit_message"
 
 # Empuja los cambios al repositorio remoto
-git push origin "$branch_name"
+git push origin "$branch_name" --force --all
 
 # Verifica si el push fue exitoso
 if [ $? -ne 0 ]; then
